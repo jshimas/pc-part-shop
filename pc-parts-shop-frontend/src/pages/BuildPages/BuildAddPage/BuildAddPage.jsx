7d6b2eccbf27d3efd4edf05f147fdfd24a30078c
ArbatosPakelis <tomasjuodelis123@gmail.com>
2022-11-08 00:24:56 +0200

    Build pages first designs

--- pc-parts-shop-frontend/src/pages/BuildPages/BuildAddPage/BuildAddPage.jsx
+++ pc-parts-shop-frontend/src/pages/BuildPages/BuildAddPage/BuildAddPage.jsx
@@ -1,9 +1,31 @@
 import * as React from "react";
+import  "./buildAddPage.css";
+import { TextField } from "@mui/material";
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function BuildAddPage() {
   return (
     <div>
-      <h3>Build Add Page</h3>
+      <div>
+          <h3 >Make your own build</h3>
+      </div>
+      <div>
+        <TextField
+          hiddenLabel
+          id="filled-hidden-label-normal"
+          defaultValue="enter build name"
+          variant="filled"
+        />
+      </div>
+
+      <Button href="/builds/:id" variant="contained" endIcon={<SendIcon />}>
+      Make build
+      </Button>
+
+      <div>
+        Problems:
+      </div>
     </div>
   );
 }
--- pc-parts-shop-frontend/src/pages/BuildPages/BuildPage/BuildPage.jsx
+++ pc-parts-shop-frontend/src/pages/BuildPages/BuildPage/BuildPage.jsx
@@ -1,11 +1,94 @@
 import * as React from "react";
 import { useParams } from "react-router-dom";
+import  "./buildPage.css";
+import { Typography } from "@mui/material";
 
 export default function BuildPage() {
   const { id } = useParams();
   return (
     <div>
-      <h3>Build Page. Build ID: {id}</h3>
+      <div>
+        <center>
+          <Typography
+          variant="h3"
+          sx={{ borderBottom: "1px solid gray", pb: 1, my: 4 }}
+          >
+            Build Page. Build ID: {id}
+          </Typography>
+        </center>
+      </div>
+      <center>
+      <table>
+        <thread>
+          <tr>
+            <th scope="col" width="13%">Component</th>
+            <th scope="col" width="9%">Product</th>
+            <th scope="col" width="47%">Title</th>
+            <th scope="col" width="7%">Price</th>
+            <th scope="col" width="18%">Product Link</th>
+            <th scope="col" width="7%">Remove</th>
+          </tr>
+          <tbody>
+          <tr class="items">
+            <td class="component" >
+              <b><a href="/">Processor</a></b>
+            </td>
+            <td class="select-comp" colspan="7">
+              <a class="comp-button" href="/">ADD Component</a>
+            </td>
+          </tr>
+          <tr class="items">
+            <td class="component">
+              <b><a href="/">Motherboard</a></b>
+            </td>
+            <td class="select-comp" colspan="7">
+              <a class="comp-button" href="/">ADD Component</a>
+            </td>
+          </tr>
+          <tr class="items">
+            <td class="component">
+              <b><a href="/">CPU Cooler</a></b>
+            </td>
+            <td class="select-comp" colspan="7">
+              <a class="comp-button" href="/">ADD Component</a>
+            </td>
+          </tr>
+          <tr class="items">
+            <td class="component">
+              <b><a href="/">GPU</a></b>
+            </td>
+            <td class="select-comp" colspan="7">
+              <a class="comp-button" href="/">ADD Component</a>
+            </td>
+          </tr>
+          <tr class="items">
+            <td class="component">
+              <b><a href="/">RAM</a></b>
+            </td>
+            <td class="select-comp" colspan="7">
+              <a class="comp-button" href="/">ADD Component</a>
+            </td>
+          </tr>
+          <tr class="items">
+            <td class="component">
+              <b><a href="/">Storage</a></b>
+            </td>
+            <td class="select-comp" colspan="7">
+              <a class="comp-button" href="/">ADD Component</a>
+            </td>
+          </tr>
+          <tr class="items">
+            <td class="component">
+              <b><a href="/">Power suply</a></b>
+            </td>
+            <td class="select-comp" colspan="7">
+              <a class="comp-button" href="/">ADD Component</a>
+            </td>
+          </tr>
+          </tbody>
+        </thread>
+      </table>
+      </center>
     </div>
   );
 }
